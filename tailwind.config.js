19e4b8064be05704b70312e936af2de37aa3b998
Jake <jakewarrenblack01@gmail.com>
2023-02-19 19:45:07 +0000

    Convert diff-came-engine vanilla js example to use react. Detecting motion exceeding threshold of 100.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {},  // no options to configure
+    variants: { // all the following default to ['responsive']
+      imageRendering: ['responsive'],
+    },
+    plugins: [
+      require('tailwindcss-image-rendering')(), // add this to allow pixelated style canvas
+    ],
 }
