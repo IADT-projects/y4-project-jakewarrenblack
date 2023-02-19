8830df7f2d960063497cf9ea45cedef038ef8916
Jake <jakewarrenblack01@gmail.com>
2023-02-19 20:56:26 +0000

    Capture image of last detected movement. Minor styling update.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useRef, useState} from "react";
-// import 'webrtc-adapter'
 import DiffCamEngine from "diff-cam-engine";
 
 function App() {
-    const [score, setScore] = useState(0)
+    const [captureData, setCaptureData] = useState(null)
     const motion = useRef(null)
     const video = useRef(null)
     const scoreEl = useRef(null)
+    const [capturedImage, setCapturedImage] = useState('')
 
     useEffect(() => {
         DiffCamEngine.init({
@@ -14,30 +14,44 @@ function App() {
             motionCanvas: motion.current,
             initSuccessCallback: () => DiffCamEngine.start(),
             initErrorCallback: () => alert('Something went wrong'),
-            captureCallback: payload => setScore(payload.score)
+            captureCallback: payload => setCaptureData(payload) // payload.imageData contains an image in the Uint8ClampedArray format
         });
     }, [])
 
     useEffect(() => {
-        if(score > 100){
+        if(captureData?.score > 100){
             alert('Movement')
-            setScore(0)
+
+            // TODO: Send this same image data to the backend for storage on s3. We can use it for performing inference with our ML model.
+            setCapturedImage(captureData.getURL())
         }
-    }, [score])
+    }, [captureData?.score])
+
 
   return (
-    <div>
-        <figure>
-            <video className={'bg-black'} ref={video}></video>
-            <figcaption>Live Video</figcaption>
-        </figure>
+    <div className={'flex justify-center items-center flex-col w-[800px] [&>*]:w-full m-auto'}>
+        <div className={'[&>*]:w-full flex'}>
+            <figure>
+                <figcaption>Live Video</figcaption>
+                <video className={'bg-black w-full h-full'} ref={video}></video>
+            </figure>
 
-        <figure>
-            <canvas ref={motion} className={'w-1/4 h-1/4 rendering-pixelated bg-black'}></canvas>
-            <figcaption>
-                Motion Heatmap<br/>
-                Score: <span ref={scoreEl}>{score.toString()}</span>
-            </figcaption>
+            <figure>
+                <figcaption className={'flex'}>
+                    <strong className={'mr-2'}>Motion Heatmap</strong>
+                    Movement Threshold: <span ref={scoreEl}>{captureData?.score}</span>
+                </figcaption>
+                <canvas ref={motion} className={'w-full h-full rendering-pixelated bg-black'}></canvas>
+            </figure>
+        </div>
+
+        <figure className={'mt-5 w-full h-full'}>
+            { capturedImage &&
+                <figure>
+                    <figcaption>Last detected movement:</figcaption>
+                    <img className={'w-full h-full'} alt={'Screenshot of motion capture'} src={capturedImage}/>
+                </figure>
+            }
         </figure>
     </div>
   );
