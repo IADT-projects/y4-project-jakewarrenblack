4d95c6163507de29db75c293b04e6c6f54c77f3d
jakewarrenblack <jake.black@sonalake.com>
2023-02-27 18:30:20 +0000

    Removing more unnecessary parts leftover from flask template

--- backend/flask_camera_server/camera.py
+++ backend/flask_camera_server/camera.py
@@ -4,14 +4,8 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
         self.video = cv2.VideoCapture(0)
         self.model = YOLO('../yolov8/models/animals.pt')
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
 
     def __del__(self):
         self.video.release()
@@ -19,10 +13,10 @@ class VideoCamera(object):
     def get_frame(self):
         ret, frame = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
+        # so we must encode it into JPEG in order to correctly display the video stream.
         # Run inference on the frame using YOLO
-        results = self.model.predict(frame, show=False,conf=0.5)  # Running inference and returning the result from get_frame
+        results = self.model.predict(frame, show=False,conf=0.5)  # Running inference and returning the result from
+        # get_frame
 
         ret, jpeg = cv2.imencode('.jpg', frame)
 
--- backend/flask_camera_server/server.py
+++ backend/flask_camera_server/server.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template, Response, jsonify, request
 from camera import VideoCamera
-import cv2
-import threading
-from ultralytics import YOLO
-import numpy as np
 
 app = Flask(__name__)
 
 video_camera = None
-global_frame = None
 
 
 @app.route('/')
@@ -17,7 +12,7 @@ def index():
 
 
 def gen(camera):
-    global global_frame, video_camera
+    global video_camera
 
     if video_camera is None:
         video_camera = VideoCamera()
@@ -25,15 +20,10 @@ def gen(camera):
     while True:
         frame, results = camera.get_frame()
 
-        if frame is not None:
-            global_frame = frame
-            print('RESULTS', results)  # printing result of YOLOv8 inference, returned along with our jpeg
+        print('RESULTS', results)  # printing result of YOLOv8 inference, returned along with our jpeg
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        else:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/video_feed')
