97a69840a382f343140b66513cba10032150f7b9
Jake <jakewarrenblack01@gmail.com>
2023-02-28 21:32:39 +0000

    YOLO labels and bounding boxes are now appearing in the live webacm feed

--- backend/flask_camera_server/camera.py
+++ backend/flask_camera_server/camera.py
@@ -2,18 +2,11 @@ from ultralytics import YOLO
 import cv2
 import time
 
-
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
         self.model = YOLO('../yolov8/models/animals.pt')
-        self.target_size = (
-        320, 240)  # 240p - resizing frame before providing it to YOLO, trying to speed up inference.
-        # self.motion_detected = False
-        # self.last_inference_time = time.time()
-
-        # Using opencv background subtractor to detect motion
-        # self.motionDetector = cv2.createBackgroundSubtractorMOG2()
+        self.target_size = (320, 240)  # 240p - resizing frame before providing it to YOLO, trying to speed up inference.
 
     def get_frame(self):
         ret, frame = self.video.read()
@@ -21,25 +14,8 @@ class VideoCamera(object):
         # so we must encode it into JPEG in order to correctly display the video stream.
         # Run inference on the frame using YOLO
 
-        # Now using the background subtraction to detect motion
-        # foreground_mask = self.motionDetector.apply(frame)
-
-        # num_pixels = foreground_mask.shape[0] * foreground_mask.shape[1]
-
-        # num_fg_pixels = cv2.countNonZero(foreground_mask) # Number of changed pixels
-
-        # if num_fg_pixels / num_pixels > 0.1: # If more than 10% of pixels are foreground
-        # Adjust this percentage to change motion sensitivity
-        # self.motion_detected = True
-
-        # Run inference if motion is detected and at least 1 second has passed since the last inference
-        # if self.motion_detected and time.time() - self.last_inference_time > 1:
-
-        # resize image
-        YOLOFrame = cv2.resize(frame, (224, 224))
-
         # Get the predicted bounding boxes and class labels for the image
-        results = self.model.predict(YOLOFrame, conf=0.5)
+        results = self.model.predict(frame, conf=0.5)
 
         # Loop over the list of dictionaries and extract the bounding boxes and class labels for each image in the batch
         for result in results:
@@ -53,13 +29,8 @@ class VideoCamera(object):
 
                     x1, y1, x2, y2 = map(int, bbox[:4])
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    cv2.putText(frame, labels[int(label)], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-            # self.last_inference_time = time.time()
-            # self.motion_detected = False  # Reset motion detection flag
-
-        # else:
-        #     results = None
+                    # Access the last index of the bbox array. The format of bbox is [x1, y1, x2, y2, inference certainty, label index]
+                    cv2.putText(frame, labels[int(bbox[-1])], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         ret, jpeg = cv2.imencode('.jpg', frame)
 
