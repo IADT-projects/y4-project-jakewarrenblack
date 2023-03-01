a9b9187528213bdec70f5d0e690cb25dccc19b68
Jake <jakewarrenblack01@gmail.com>
2023-03-01 17:20:43 +0000

    Trying a multithreading approach to run opencv, inference, and socketio server at the same time. Not working yet.

--- backend/flask_camera_server/server.py
+++ backend/flask_camera_server/server.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template, Response, jsonify, request
+from flask import Flask, Response
 from camera import VideoCamera
+from flask_socketio import SocketIO
+from flask_cors import CORS, cross_origin
+import cv2
+import time
+import queue
+import threading
+
 
 app = Flask(__name__)
 
+cors = CORS(app, resources={r"/": {"origins": ""}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 video_camera = None
 
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=True, engineio_logger=True)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+# Use a queue to communicate between the main thread and the Flask-SocketIO server
+frame_queue = queue.Queue()
 
 
 def gen(camera):
@@ -20,16 +31,56 @@ def gen(camera):
     while True:
         frame, results = camera.get_frame()
 
-        #print('RESULTS', results)  # printing result of YOLOv8 inference, returned along with our jpeg
+        # Add the frame and results to the queue
+        frame_queue.put((frame, results))
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/video_feed')
+@cross_origin()
 def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+# Define a function that runs the Flask-SocketIO server in a separate thread
+def socketio_server():
+    socketio.run(app, host='0.0.0.0', port=5000)
+
+
+# Define a function that reads from the queue and emits the data to the clients
+def emit_frames():
+    while True:
+        # Block until there is data in the queue
+        frame, results = frame_queue.get()
+
+        # Emit the frame data to the client
+        socketio.emit('frame', {'frame': 'hello world'})
+
+        # Sleep for a short amount of time to allow other events to be processed
+        time.sleep(0.01)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    # Start the Flask-SocketIO server in a separate thread
+    socketio_thread = threading.Thread(target=socketio_server)
+    socketio_thread.daemon = True
+    socketio_thread.start()
+
+    # Start a separate thread that reads from the queue and emits frames to the clients
+    emit_thread = threading.Thread(target=emit_frames)
+    emit_thread.daemon = True
+    emit_thread.start()
+
+    # Start the main OpenCV loop
+    video_camera = VideoCamera()
+    while True:
+        # Read a frame from the camera and process it
+        frame, results = video_camera.get_frame()
+
+        # Add the frame and results to the queue
+        frame_queue.put((frame, results))
+
+        # Sleep for a short amount of time to allow other events to be processed
+        time.sleep(0.01)
