20f3547084c064050a4b3fb17d30268c722df1dd
Jake <jakewarrenblack01@gmail.com>
2023-03-11 17:17:42 +0000

    Use real QR code on frontend

--- frontend/src/pages/pairing.jsx
+++ frontend/src/pages/pairing.jsx
@@ -1,12 +1,23 @@
 import {Oval} from 'react-loader-spinner'
 import qr from '../Assets/qr_code.jpg'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export const Pairing = () => {
+    const [code, setCode] = useState()
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/api/pair/`).then((res) => {
+            setCode(res.data)
+        }).catch((e) => {
+            console.log(e)
+        })
+    }, [])
 
     return (
         <div className={'px-1'}>
             <h1 className={'text-white text-4xl text-center font-semibold mb-5'}>Show this code to the Raspberry Pi</h1>
-            <img className={'p-2'} src={qr}/>
+            <img className={'p-2 w-full'} src={code || qr}/>
             <div className={'flex justify-center mt-5 flex-col items-center'}>
                 <Oval
                     height={80}
