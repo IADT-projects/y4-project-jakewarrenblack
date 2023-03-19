8c2c55a6252a4f3fa6fa4473b188eb835a5d2f6f
jakewarrenblack <jake.black@sonalake.com>
2023-03-19 23:08:30 +0000

    Can now use the 'activate buzzer' button to buzz the pi

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,7 +1,7 @@
-export const Button = ({btnText}) => {
+export const Button = ({btnText, onClick}) => {
     return (
         <div>
-            <button className={'bg-navyBtn text-cyberYellow rounded-md py-4 font-medium text-center w-full my-1 text-2xl mt-3'}>
+            <button onClick={onClick} className={'bg-navyBtn text-cyberYellow rounded-md py-4 font-medium text-center w-full my-1 text-2xl mt-3'}>
                 {btnText}
             </button>
         </div>
