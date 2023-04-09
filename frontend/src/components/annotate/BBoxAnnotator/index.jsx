fb47397e123a09680234af334be679bd81cc62d9
jakewarrenblack <jakewarrenblack01@gmail.com>
2023-04-09 13:35:53 +0100

    Improve readability of bbox annotator index file, remove some redundant state values, use tailwind in some places

--- frontend/src/components/annotate/BBoxAnnotator/index.jsx
+++ frontend/src/components/annotate/BBoxAnnotator/index.jsx
@@ -3,6 +3,7 @@ import { createUseStyles } from 'react-jss';
 import { v4 as uuid } from 'uuid';
 import BBoxSelector from '../BBoxSelector';
 import LabelBox from '../LabelBox';
+
 const useStyles = createUseStyles({
     bBoxAnnotator: {
         cursor: 'crosshair',
@@ -12,6 +13,7 @@ const useStyles = createUseStyles({
         backgroundSize: '100%',
     },
 });
+
 const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, labels, onChange }, ref) => {
     const classes = useStyles();
     const [pointer, setPointer] = useState(null);
@@ -36,32 +38,34 @@ const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, lab
 
 
     const [status, setStatus] = useState('free');
-    const [bBoxAnnotatorStyle, setBboxAnnotatorStyle] = useState({});
     const [imageFrameStyle, setImageFrameStyle] = useState({});
     const bBoxAnnotatorRef = useRef(null);
     const labelInputRef = useRef(null);
+
     useEffect(() => {
         const maxWidth = bBoxAnnotatorRef.current?.offsetWidth || 1;
         const imageElement = new Image();
         imageElement.src = url;
+
         imageElement.onload = function () {
             const width = imageElement.width;
             const height = imageElement.height;
             setMultiplier(width / maxWidth);
-            setBboxAnnotatorStyle({
-                width: width / multiplier,
-                height: height / multiplier,
-            });
+
             setImageFrameStyle({
                 backgroundImageSrc: imageElement.src,
                 width: width / multiplier,
                 height: height / multiplier,
             });
         };
+
         imageElement.onerror = function () {
             throw 'Invalid image URL: ' + url;
         };
+
     }, [url, multiplier, bBoxAnnotatorRef]);
+
+
     const crop = (pageX, pageY) => {
         return {
             x: bBoxAnnotatorRef.current && imageFrameStyle.width
@@ -72,9 +76,13 @@ const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, lab
                 : 0,
         };
     };
+
+
     const updateRectangle = (pageX, pageY) => {
         setPointer(crop(pageX, pageY));
     };
+
+
     useEffect(() => {
         const mouseMoveHandler = (e) => {
             switch (status) {
@@ -85,6 +93,8 @@ const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, lab
         window.addEventListener('mousemove', mouseMoveHandler);
         return () => window.removeEventListener('mousemove', mouseMoveHandler);
     }, [status]);
+
+
     useEffect(() => {
         const mouseUpHandler = (e) => {
             switch (status) {
@@ -97,12 +107,16 @@ const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, lab
         window.addEventListener('mouseup', mouseUpHandler);
         return () => window.removeEventListener('mouseup', mouseUpHandler);
     }, [status, labelInputRef]);
+
+
     const addEntry = (label) => {
         setEntries([...entries, { ...rect, label, id: uuid(), showCloseButton: false }]);
         setStatus('free');
         setPointer(null);
         setOffset(null);
     };
+
+
     const mouseDownHandler = (e) => {
         switch (status) {
             case 'free':
@@ -114,6 +128,8 @@ const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, lab
                 }
         }
     };
+
+
     const rectangle = () => {
         const x1 = offset && pointer ? Math.min(offset.x, pointer.x) : 0;
         const x2 = offset && pointer ? Math.max(offset.x, pointer.x) : 0;
@@ -126,73 +142,68 @@ const BBoxAnnotator = React.forwardRef(({ url, borderWidth = 2, inputMethod, lab
             height: y2 - y1 + 1,
         };
     };
+
+
     useImperativeHandle(ref, () => ({
         reset() {
             setEntries([]);
         },
     }));
-    const rect = rectangle();
-    return (<div className={classes.bBoxAnnotator} style={{
-        width: `auto`,
-        height: `100%`,
-    }} ref={bBoxAnnotatorRef} onMouseDown={mouseDownHandler}>
-            <div className={classes.imageFrame} style={{
-        width: `auto`,
-        height: `100%`,
-        backgroundRepeat: 'no-repeat',
-        backgroundImage: `url(${imageFrameStyle.backgroundImageSrc})`,
-    }}>
-                {status === 'hold' || status === 'input' ? <BBoxSelector rectangle={rect}/> : null}
-                {status === 'input' ? (<LabelBox inputMethod={inputMethod} top={rect.top + rect.height + borderWidth} left={rect.left - borderWidth} labels={labels} onSubmit={addEntry} ref={labelInputRef}/>) : null}
-                {entries.map((entry, i) => (<div style={{
-        border: `${borderWidth}px solid rgb(255,0,0)`,
-        position: 'absolute',
-        top: `${entry.top - borderWidth}px`,
-        left: `${entry.left - borderWidth}px`,
-        width: `${entry.width}px`,
-        height: `${entry.height}px`,
-        color: 'rgb(255,0,0)',
-        fontFamily: 'monospace',
-        fontSize: 'small',
-    }} key={i} onMouseOver={() => setEntries((prevEntries) => prevEntries.map((e) => (e.id === entry.id ? { ...e, showCloseButton: true } : e)))} onMouseLeave={() => setEntries((prevEntries) => prevEntries.map((e) => (e.id === entry.id ? { ...e, showCloseButton: false } : e)))}>
-                        {entry.showCloseButton ? (<div style={{
-        position: 'absolute',
-        top: '-8px',
-        right: '-8px',
-        width: '16px',
-        height: '0',
-        padding: '16px 0 0 0',
-        overflow: 'hidden',
-        color: '#fff',
-        backgroundColor: '#030',
-        border: '2px solid #fff',
-        borderRadius: '18px',
-        cursor: 'pointer',
-        userSelect: 'none',
-        textAlign: 'center',
-    }} onMouseDown={(e) => {
-        e.stopPropagation();
-    }} onClick={() => {
-        setEntries(entries.filter((e) => e.id !== entry.id));
-    }}>
-                                <div style={{
-        display: 'block',
-        textAlign: 'center',
-        width: '16px',
-        position: 'absolute',
-        top: '-2px',
-        left: '0',
-        fontSize: '16px',
-        lineHeight: '16px',
-        fontFamily: '"Helvetica Neue", Consolas, Verdana, Tahoma, Calibri, ' +
-            'Helvetica, Menlo, "Droid Sans", sans-serif',
-    }}>
-                                    &#215;
-                                </div>
-                            </div>) : null}
-                        <div style={{ overflow: 'hidden' }}>{entry.label}</div>
-                    </div>))}
+
+
+    const entryItem = (entry, i) => {
+        return (
+            <div className={'absolute text-red-500 border-red-500 border-2 absolute'} style={{
+                top: `${entry.top - borderWidth}px`,
+                left: `${entry.left - borderWidth}px`,
+                width: `${entry.width}px`,
+                height: `${entry.height}px`}}
+                key={i}
+                onMouseOver={() => setEntries((prevEntries) => prevEntries.map((e) => (e.id === entry.id ? { ...e, showCloseButton: true } : e)))}
+                onMouseLeave={() => setEntries((prevEntries) => prevEntries.map((e) => (e.id === entry.id ? { ...e, showCloseButton: false } : e)))}>
+
+                    {entry.showCloseButton &&
+                     <div
+                         className={'border-white border-2 select-none text-white text-center cursor-pointer overflow-hidden bg-[#030] absolute'}
+                         style={{
+                            top: '-8px',
+                            right: '-8px',
+                            width: '16px',
+                            height: '0',
+                            padding: '16px 0 0 0',
+                            borderRadius: '18px',
+                        }}
+                         onMouseDown={(e) => {
+                            e.stopPropagation();
+                        }}
+                         onClick={() => {
+                            setEntries(entries.filter((e) => e.id !== entry.id));
+                        }}>
+                        <div className={'block text-center absolute'} style={{width: '16px', top: '-2px', left: '0', fontSize: '16px', lineHeight: '16px',}}>
+                            &#215;
+                        </div>
+                    </div>}
+
+                    <div style={{ overflow: 'hidden' }}>
+                        {entry.label}
+                    </div>
+
             </div>
-        </div>);
+        )
+    }
+
+    const rect = rectangle();
+
+    return (
+        <div className={classes.bBoxAnnotator} style={{width: `auto`, height: `100%`,}} ref={bBoxAnnotatorRef} onMouseDown={mouseDownHandler}>
+            <div className={classes.imageFrame} style={{width: `auto`, height: `100%`, backgroundRepeat: 'no-repeat', backgroundImage: `url(${imageFrameStyle.backgroundImageSrc})`,}}>
+                {status === 'hold' || status === 'input' ? <BBoxSelector rectangle={rect}/> : null}
+
+                {status === 'input' ? (<LabelBox inputMethod={inputMethod} top={rect.top + rect.height + borderWidth} left={rect.left - borderWidth} labels={labels} onSubmit={addEntry} ref={labelInputRef}/>) : null}
+
+                {entries.map((entry, i) => entryItem(entry, i))}
+            </div>
+        </div>
+    )
 });
 export default BBoxAnnotator;
